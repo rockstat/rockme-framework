04dedd373a9f3b2ae9dc8cdf0b5cd7cdfdd01dbd
Dmitry Rodin <madiedinro@gmail.com>
2024-06-06 01:30:50 +0300

    removed debug logs

--- src/AppDeps.ts
+++ src/AppDeps.ts
@@ -1,17 +1,11 @@
 class AppDeps {
     deps: Map<string, any> = new Map();
-
-    constructor() {
-        console.log('constructing AppDeps');
-    }
-
+    
     setDep(name: string, obj: any): void {
-        console.debug('setting dep ', name); 
         this.deps.set(name, obj);
     }
 
     getDep(name: string): any {
-        console.debug('getting dep ', name); 
         return this.deps.get(name);
     }
 }
